913565f24315d420de401aa4dc8b3bab4b0ac72b
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-03-28 14:42:00 +0100

    add footer with source links

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -101,10 +101,22 @@ export default {
                 <h3>Synonyms</h3>
                 <h3 v-for="synonym in wordResult.meanings[1].synonyms">{{ synonym }}</h3>
             </div>
-
-
         </div>
     </main>
+
+    <footer>
+        <div v-if="wordExist" class="container p-inline">
+            <div class="divider">
+                <div class="line"></div>
+            </div>
+            <div class="source">
+                <h4>Source</h4>
+                <h4 v-for="source in wordResult.sourceUrls"><a target="_blank" :href="source">{{ source }}</a></h4>
+            </div>
+        </div>
+    </footer>
+
+
 </template>
 
 <style></style>
