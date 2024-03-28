5bd323d1bf3e461bf4e3276c9a9396bb9c850e1e
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-03-28 15:04:07 +0100

    add store file and footer component

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,54 +1,30 @@
 <script>
-import axios from 'axios';
+
+import { store } from '../store.js';
 
 export default {
     name: 'AppMain',
     data() {
         return {
-            searchText: '',
-            wordResult: [],
-            wordExist: false,
-            errorMessage: false
+            store
         }
     },
-    methods: {
-        search(inputWord) {
-            if (inputWord !== '') {
-                axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${inputWord}`)
-                    .then(res => {
-                        this.wordResult = res.data[0];
-                        this.searchText = '';
-                        this.wordExist = true;
-                        this.errorMessage = false;
-                        console.log(this.wordResult);
-                    })
-                    .catch(err => {
-                        console.error('Error fetching word:', err.name);
-                        this.wordExist = false;
-                        this.errorMessage = true;
-                    });
-            }
-        },
-        cleanError() {
-            this.searchText == '' && (this.errorMessage = false);
-        }
-    }
 }
 </script>
 
 <template>
     <main>
         <div class="container">
-            <input @keyup="cleanError" @keyup.enter="search(searchText)" v-model="searchText"
-                :class="errorMessage ? 'border_error' : 'border_default'" class="input" type="text"
+            <input @keyup="store.cleanError" @keyup.enter="store.search(store.searchText)" v-model="store.searchText"
+                :class="store.errorMessage ? 'border_error' : 'border_default'" class="input" type="text"
                 placeholder="Search for any word...">
-            <span class="error_message" v-show="errorMessage">Word not found...</span>
+            <span class="error_message" v-show="store.errorMessage">Word not found...</span>
         </div>
-        <div v-if="wordExist" class="container p-inline">
+        <div v-if="store.wordExist" class="container p-inline">
             <div class="word">
                 <div>
-                    <h1>{{ wordResult.word }}</h1>
-                    <h2>{{ wordResult.phonetic }}</h2>
+                    <h1>{{ store.wordResult.word }}</h1>
+                    <h2>{{ store.wordResult.phonetic }}</h2>
                 </div>
                 <div class="audio_btn">
                     <i class="fa-solid fa-play"></i>
@@ -57,14 +33,14 @@ export default {
 
             <div class="divider">
                 <!-- noun -->
-                <h3>{{ wordResult.meanings[0].partOfSpeech }}</h3>
+                <h3>{{ store.wordResult.meanings[0].partOfSpeech }}</h3>
                 <div class="line"></div>
             </div>
 
             <div class="meaning">
                 <h3>Meaning</h3>
                 <ul>
-                    <li v-for="definition in wordResult.meanings[0].definitions">
+                    <li v-for="definition in store.wordResult.meanings[0].definitions">
                         <span>
                             {{ definition.definition }}
                         </span>
@@ -72,21 +48,21 @@ export default {
                 </ul>
             </div>
 
-            <div v-show="wordResult.meanings[0].synonyms.length > 0" class="synonyms">
+            <div v-show="store.wordResult.meanings[0].synonyms.length > 0" class="synonyms">
                 <h3>Synonyms</h3>
-                <h3 v-for="synonym in wordResult.meanings[0].synonyms">{{ synonym }}</h3>
+                <h3 v-for="synonym in store.wordResult.meanings[0].synonyms">{{ synonym }}</h3>
             </div>
 
             <div class="divider">
                 <!-- verb -->
-                <h3>{{ wordResult.meanings[1].partOfSpeech }}</h3>
+                <h3>{{ store.wordResult.meanings[1].partOfSpeech }}</h3>
                 <div class="line"></div>
             </div>
 
             <div class="meaning">
                 <h3>Meaning</h3>
                 <ul>
-                    <li v-for="definition in wordResult.meanings[1].definitions">
+                    <li v-for="definition in store.wordResult.meanings[1].definitions">
                         <span>
                             {{ definition.definition }}
                         </span>
@@ -97,26 +73,12 @@ export default {
                 </ul>
             </div>
 
-            <div v-show="wordResult.meanings[1].synonyms.length > 0" class="synonyms">
+            <div v-show="store.wordResult.meanings[1].synonyms.length > 0" class="synonyms">
                 <h3>Synonyms</h3>
-                <h3 v-for="synonym in wordResult.meanings[1].synonyms">{{ synonym }}</h3>
+                <h3 v-for="synonym in store.wordResult.meanings[1].synonyms">{{ synonym }}</h3>
             </div>
         </div>
     </main>
-
-    <footer>
-        <div v-if="wordExist" class="container p-inline">
-            <div class="divider">
-                <div class="line"></div>
-            </div>
-            <div class="source">
-                <h4>Source</h4>
-                <h4 v-for="source in wordResult.sourceUrls"><a target="_blank" :href="source">{{ source }}</a></h4>
-            </div>
-        </div>
-    </footer>
-
-
 </template>
 
 <style></style>
